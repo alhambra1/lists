d416003075a4238a307abbbc19ada210f56be1eb
uu <giladbarkan@live.com>
2016-08-26 08:01:04 -0400

    added case insensitivity to username uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  validates :username, presence: true, uniqueness: true
+  validates :username, presence: true
+  validates_uniqueness_of :username, :case_sensitive => false
 
   has_many :user_lists, dependent: :destroy
   has_many :lists, through: :user_lists
