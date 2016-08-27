68a0b1e5be73f8f958a97d91a6dbffa98553bcc8
alhambra1 <gilad12@fastmail.fm>
2016-08-27 18:20:54 -0400

    Update List.js

--- public/js/app/models/List.js
+++ public/js/app/models/List.js
@@ -3,7 +3,7 @@ var List = function(list_hash){
   this.name = list_hash.name;
   this.editable = list_hash.editable;
   this.collaborators = list_hash.collaborators;
-  this.tasks = list_hash.tasks ? list_hash.tasks.map(x => new Task(x)) : [];
+  this.tasks = list_hash.tasks ? list_hash.tasks.map(function(x){ return new Task(x); }) : [];
   this.errors = list_hash.errors;
 
   this.collaboratorsWithoutCreator = function(){
